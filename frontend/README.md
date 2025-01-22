f30a0a425f524c7d764d931e1fb7f3d898415f7e
KWIZERA-CALEB <kwizeracaleb91@gmail.com>
2025-01-22 22:25:24 +0200

    chore: setup the project both frontend and backend

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+Umurava Edtech Product Challenge
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+        <Button>Just was testing Shadcn</Button>
+    </div>
+  );
+}

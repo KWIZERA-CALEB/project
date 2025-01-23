a608d8042b7f188a84585d6c34325b95bce7a312
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-01-23 11:43:32 +0200

    hero created successfully

---
+++ frontend/src/app/homepage/page.tsx
@@ -0,0 +1,13 @@
+import Hero from './sections/Hero';
+import Navbar from '../../components/Navbar';
+
+export default function page(){
+
+    return(
+    <>
+    <Navbar/>
+    <Hero/>
+    </>
+    
+)
+}

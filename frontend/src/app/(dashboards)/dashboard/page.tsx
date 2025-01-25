fc5503b711c593af0abb94cf6dac06bc88700764
KWIZERA-CALEB <kwizeracaleb91@gmail.com>
2025-01-25 11:13:57 +0200

    final touch on talent dashboard

--- frontend/src/app/(dashboards)/dashboard/page.tsx
+++ frontend/src/app/(dashboards)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import AdminNavbar from '@/components/custom/admin/AdminNavbar'
 import Link from 'next/link'
 import ChallengeCard from '@/components/custom/admin/ChallengeCard'
 import MobileSidebar from '@/components/custom/admin/MobileSidebar'
+import { Button } from "@/components/ui/button"
 
 
 
@@ -164,6 +165,17 @@ export default function AdminDashboard () {
                             </div>
                         </>
                         :
+                        <>
+                            <div className='mt-[12px] flex justify-end'>
+                                <Button className='bg-umuravaBlueColor flex flex-row space-x-[6px] items-center text-white hover:bg-umuravaBlueColor/[90%]'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height='20' width='20' fill="#fff">
+                                        <path d="M12.0003 3C17.3924 3 21.8784 6.87976 22.8189 12C21.8784 17.1202 17.3924 21 12.0003 21C6.60812 21 2.12215 17.1202 1.18164 12C2.12215 6.87976 6.60812 3 12.0003 3ZM12.0003 19C16.2359 19 19.8603 16.052 20.7777 12C19.8603 7.94803 16.2359 5 12.0003 5C7.7646 5 4.14022 7.94803 3.22278 12C4.14022 16.052 7.7646 19 12.0003 19ZM12.0003 16.5C9.51498 16.5 7.50026 14.4853 7.50026 12C7.50026 9.51472 9.51498 7.5 12.0003 7.5C14.4855 7.5 16.5003 9.51472 16.5003 12C16.5003 14.4853 14.4855 16.5 12.0003 16.5ZM12.0003 14.5C13.381 14.5 14.5003 13.3807 14.5003 12C14.5003 10.6193 13.381 9.5 12.0003 9.5C10.6196 9.5 9.50026 10.6193 9.50026 12C9.50026 13.3807 10.6196 14.5 12.0003 14.5Z"></path>
+                                    </svg>
+                                    <p>
+                                    View Profile
+                                    </p>
+                                </Button>
+                            </div>
                             <div className='md:mt-[12px] mt-[6px] w-full flex flex-col space-y-[6px] md:space-y-[0px] md:flex-row justify-between items-center md:space-x-[12px]'>
                                 <div className='bg-white h-[120px] rounded-[15px] w-full md:w-[50%] flex items-center border-solid border-[1px] border-[#E4E7EC] p-[16px]'>
                                     <div className='flex flex-row justify-between w-full items-center'>
@@ -211,6 +223,7 @@ export default function AdminDashboard () {
                                     </div>
                                 </div>
                             </div>
+                        </>
                         }
                         {/* statistics */}
                         <div className='mt-[40px]'>

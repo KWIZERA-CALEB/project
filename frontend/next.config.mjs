330f48f6fcfa8504fadef8e059f3c3bac01850c1
KWIZERA-CALEB <kwizeracaleb91@gmail.com>
2025-01-26 22:22:16 +0200

    update nextconfig.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true, // Enables App Router
-    },
-  };
-  
+  reactStrictMode: true,
+  webpack(config) {
+    // Allow imports from the `src` directory
+    config.resolve.modules.push(path.resolve('./src'));
+    return config;
+  },
+};
+
 export default nextConfig;
-  

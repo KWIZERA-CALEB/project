c3425eb1849f2cd27d97059ea0d7a1969f9cdeb4
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-01-26 11:46:07 +0200

    I implemented the challenges & hackathon page, still working on the getstarted section of homepage.

--- frontend/src/app/(client)/challenges&hackathons/page.tsx
+++ frontend/src/app/(client)/challenges&hackathons/page.tsx
@@ -67,37 +67,7 @@ const ChallengesPage = () => {
 
       <main className="container mx-auto px-4 py-16 flex-grow">
         <h1 className="text-4xl font-bold text-center mb-12">Challenges & Hackathons</h1>
-        <section className="grid grid-cols-1 lg:grid-cols-4 gap-8">
-          <div className="lg:col-span-2 bg-white shadow-md rounded-2xl p-6 flex flex-col relative">
-            <div className="text-left">
-              <p className="text-blue-600 font-bold mb-4">Step 1</p>
-              <h3 className="text-xl font-semibold mb-4">Sign up on Umurava Platform</h3>
-              <p className="text-gray-600">Submit your completed project for evaluation</p>
-            </div>
-            <div className="absolute bottom-6 right-6">
-              <img
-                src="/assets/images/step1.png"
-                alt="Step 1 Image"
-                className="rounded-lg shadow-lg w-full lg:w-3/4"
-              />
-            </div>
-          </div>
-          <div className="bg-white shadow-md rounded-2xl p-6">
-            <div className="text-left">
-              <p className="text-blue-600 font-bold mb-4">Step 3</p>
-              <h3 className="text-xl font-semibold mb-4">Register and Participate</h3>
-              <p className="text-gray-600">Sign up for the challenge and start working on the project.</p>
-            </div>
-          </div>
-          <div className="bg-white shadow-md rounded-2xl p-6">
-            <div className="text-left">
-              <p className="text-blue-600 font-bold mb-4">Step 4</p>
-              <h3 className="text-xl font-semibold mb-4">Submit your work</h3>
-              <p className="text-gray-600">Submit your completed project for evaluation</p>
-            </div>
-          </div>
-        </section>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
           {challenges.map((challenge, index) => (
             <ChallengeCard
               key={index}

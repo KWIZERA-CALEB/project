ae43e3292462e3d4c296184e1df5d8d7fb7fb4aa
d3cxty <santafate250@gmail.com>
2025-01-27 08:23:37 +0200

    i had to fix the next config error

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,15 +4,8 @@ import path from 'path';
 const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
-    // Add src directory to resolve modules
+    // Allow imports from the `src` directory
     config.resolve.modules.push(path.resolve('./src'));
-
-    // Ensure backward compatibility for other modules (if required)
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'), // Optional: Short alias '@'
-    };
-
     return config;
   },
 };

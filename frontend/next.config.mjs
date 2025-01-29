323e764432f2c3db5e2d867651a8c38446015d5b
KWIZERA-CALEB <kwizeracaleb91@gmail.com>
2025-01-29 21:29:31 +0200

    edited next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,20 @@
-/** @type {import('next').NextConfig} */
-import path from 'path';
+// /** @type {import('next').NextConfig} */
+// import path from 'path';
 
+// const nextConfig = {
+//   reactStrictMode: true,
+//   webpack(config) {
+//     config.resolve.modules.push(path.resolve('./src'));
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    // Allow imports from the `src` directory
-    config.resolve.modules.push(path.resolve('./src'));
-    return config;
-  },
+  pageExtensions: ['tsx', 'ts'], // To handle TypeScript files
 };
 
 export default nextConfig;
